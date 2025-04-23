cfaad5c798cc17bd31cb4f14f8052ee9062e4ceb
kaaimu <tryadi.tab@gmail.com>
2025-04-23 12:15:54 +0700

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
-
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html' // optional, untuk SPA
+    }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/NAMA_REPO' : ''
+    }
+  }
+}
