ed3f511acb3a4b60cc899f9bed917c11d923f120
kaaimu <tryadi.tab@gmail.com>
2025-04-23 12:33:38 +0700

    Initial commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html' // optional, untuk SPA
-    }),
-	paths: {
-		base: process.env.NODE_ENV === 'production' ? '/portfolio' : ''
-	  }
-  }
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: ''
+		}
+	}
+};
+
+export default config;
